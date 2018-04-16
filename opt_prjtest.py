d50339a5c66812c668cee934a6dd9e408c09410d
wstcl <gaojunwtc@163.com>
2018-04-16 16:19:47 -0700

    testfile

---
+++ opt_prjtest.py
@@ -0,0 +1,216 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./") #自动下载数据到这个目录
+X_train = mnist.train.images
+X_test = mnist.test.images
+X_train = X_train.T
+X_test = X_test.T
+y_train = mnist.train.labels.astype("int")
+y_test = mnist.test.labels.astype("int")
+y_test=tf.one_hot(y_test,10,on_value=1,off_value=0,axis=0)
+y_train=tf.one_hot(y_train,10,on_value=1,off_value=0,axis=0)
+with tf.Session() as sess:
+    sess.run(y_test)
+    y_test = tf.cast(y_test, dtype=tf.int32)
+    y_test = y_test.eval()
+    sess.run(y_train)
+    y_train = tf.cast(y_train, dtype=tf.int32)
+    y_train = y_train.eval()
+
+def initialize_parameters(layer_dims):
+    np.random.seed(3)
+    parameters={}
+    L=len(layer_dims)
+
+    for l in range(1,L):
+        parameters['W'+str(l)] = np.random.randn(layer_dims[l],layer_dims[l-1])*0.01
+        parameters['b'+str(l)] = np.zeros((layer_dims[l],1))
+
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+def linear_forward(A,W,b):
+    Z = np.dot(W, A) + b
+    assert(Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+    return Z,cache
+
+def linear_activation_forward(A_prev,W,b,activation):
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+def L_model_forward(X, parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2
+    for l in range(1, L):
+        A_prev = A
+        A, cache = linear_activation_forward(A_prev,
+                                             parameters['W' + str(l)],
+                                             parameters['b' + str(l)],
+                                             activation='relu')
+        caches.append(cache)
+    AL, cache = linear_activation_forward(A,
+                                          parameters['W' + str(L)],
+                                          parameters['b' + str(L)],
+                                          activation='sigmoid')
+    caches.append(cache)
+    assert (AL.shape == (10, X.shape[1]))
+
+    return AL, caches
+
+def compute_cost(AL, Y):
+    m = Y.shape[1]
+    cost = (-1 / m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1 - Y, np.log(1 - AL)))
+    cost = np.squeeze(cost)
+    assert (cost.shape == ())
+
+    return cost
+
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+    dW = np.dot(dZ, cache[0].T) / m
+    db = np.squeeze(np.sum(dZ, axis=1, keepdims=True)) / m
+    db = np.transpose([db])# need to be cared about
+    dA_prev = np.dot(cache[1].T, dZ)
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+
+    return dA_prev, dW, db
+
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+    dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return  dA_prev, dW, db
+
+def L_model_backward(AL,Y,caches):
+    grads = {}
+    m = AL.shape[1]
+    L = len(caches)
+    Y = Y.reshape(AL.shape)
+    dAL = dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+    current_cache = caches[-1]
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_backward(sigmoid_backward(dAL, current_cache[1]),current_cache[0])
+    for l in reversed(range(L - 1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA{}".format(l + 2)],
+                                                                    current_cache,
+                                                                    "relu")
+
+        grads["dA" + str(l + 1)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+    return grads
+
+def initialize_adam(parameters):
+    L = len(parameters) // 2  # number of layers in the neural networks
+    v = {}
+    s = {}
+    for l in range(L):
+        v["dW" + str(l + 1)] = np.zeros_like(parameters["W" + str(l + 1)])
+        v["db" + str(l + 1)] = np.zeros_like(parameters["b" + str(l + 1)])
+
+        s["dW" + str(l + 1)] = np.zeros_like(parameters["W" + str(l + 1)])
+        s["db" + str(l + 1)] = np.zeros_like(parameters["b" + str(l + 1)])
+
+
+    return v, s
+
+
+def update_parameters_with_adam(parameters, grads, v, s, t, learning_rate=0.01,
+                                beta1=0.9, beta2=0.999, epsilon=1e-8):
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+    v_corrected = {}  # Initializing first moment estimate, python dictionary
+    s_corrected = {}  # Initializing second moment estimate, python dictionary
+    for l in range(L):
+        v["dW" + str(l + 1)] = beta1 * v["dW" + str(l + 1)] + (1 - beta1) * grads['dW' + str(l + 1)]
+        v["db" + str(l + 1)] = beta1 * v["db" + str(l + 1)] + (1 - beta1) * grads['db' + str(l + 1)]
+
+        v_corrected["dW" + str(l + 1)] = v["dW" + str(l + 1)] / (1 - np.power(beta1, t))
+        v_corrected["db" + str(l + 1)] = v["db" + str(l + 1)] / (1 - np.power(beta1, t))
+
+        s["dW" + str(l + 1)] = beta2 * s["dW" + str(l + 1)] + (1 - beta2) * np.power(grads['dW' + str(l + 1)], 2)
+        s["db" + str(l + 1)] = beta2 * s["db" + str(l + 1)] + (1 - beta2) * np.power(grads['db' + str(l + 1)], 2)
+
+        s_corrected["dW" + str(l + 1)] = s["dW" + str(l + 1)] / (1 - np.power(beta2, t))
+        s_corrected["db" + str(l + 1)] = s["db" + str(l + 1)] / (1 - np.power(beta2, t))
+
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * v_corrected[
+            "dW" + str(l + 1)] / np.sqrt(s["dW" + str(l + 1)] + epsilon)
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * v_corrected[
+            "db" + str(l + 1)] / np.sqrt(s["db" + str(l + 1)] + epsilon)
+
+
+    return parameters, v, s
+
+layers_dims = [784, 10, 10, 10, 10]
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=500, print_cost=True):
+    L = len(layers_dims)
+    costs = []
+    t = 0
+
+    parameters = initialize_parameters(layers_dims)
+    v, s = initialize_adam(parameters)
+    for i in range(0, num_iterations):
+
+        AL, caches = L_model_forward(X, parameters)
+
+        cost = compute_cost(AL, Y)
+
+        grads = L_model_backward(AL, Y, caches)
+        t = t + 1
+        parameters = update_parameters_with_adam(parameters, grads, v, s, t, learning_rate=0.01,
+                                beta1=0.9, beta2=0.999, epsilon=1e-8)
+
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+    return parameters
+
+def sigmoid(Z):
+    A = 1 / (1 + np.exp(-Z))
+    cache = Z
+    return A, cache
+
+def relu(Z):
+    A = np.maximum(0, Z)
+    assert (A.shape == Z.shape)
+    cache = Z
+    return A, cache
+
+def relu_backward(dA, cach):
+    Z = cach
+    dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
+    dZ[Z <= 0] = 0
+    assert (dZ.shape == Z.shape)
+    return dZ
+
+def sigmoid_backward(dA, cach):
+    Z = cach
+    s = 1 / (1 + np.exp(-Z))
+    dZ = dA * s * (1 - s)
+    assert (dZ.shape == Z.shape)
+    return dZ
+
+
+
+L_layer_model(X_train, y_train, layers_dims, learning_rate=0.0075, num_iterations=500, print_cost=False)
